ae5d481e6a2d7a0a0eccd33bc20cfa4b3d1fbb0a
Muhammad Amirul <mirulid@gmail.com>
2024-07-22 20:02:45 +0700

    first commit

---
+++ src/test/java/homework/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
